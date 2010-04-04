6c856fb9c41b9465c5fde15be42a9ad3f3ac0bef
Bogolt <dmitrii.sernii@gmail.com>
2010-04-05 00:15:18 +0300

    GameRect class allows splitting into regions according to game field

--- src/geometry.py
+++ src/geometry.py
@@ -1,8 +1,13 @@
 class Coord:
+	
+	maxSize = 1000
 	'two-dimentional coordinate on map'
 	def __init__(self, x = 0, y = 0):
 		self.x = x
 		self.y = y
+
+	def is_valid(self):
+		return self.x != 0 and self.y != 0
 
 	def __eq__(self, c):
 		'compare with the same object type for equality'
@@ -43,29 +48,80 @@ class Coord:
 	def __sub__(self, c):
 		'add a simple tuple object'
 		return Coord(self.x - c[0], self.y - c[1])
+	
+	def __str__(self):
+		return '%s:%s'%(self.x,self.y)
 		
 	def __getitem__(self, index):
 		'return coordinates like if they are tuple pair'
 		if index==0:
 			return self.x
 		return self.y
+		
+	def minimize(self):
+		return Coord(self.x % self.maxSize, self.y % self.maxSize)
+		
+	def shrink(self, size):
+		return Coord(min(self.x, size[0]), min(self.y, size[1]))
 
+class Rect:
+	def __init__(self, c, sz):
+		self.coord = c
+		self.sz = sz
 
 class GameRect:
+	'game rect object, it can overlap game borders, so use rects() to get a list of plain rects'
 	
 	'default galaxy size'
-	galaxySize = 1000,1000
+	galaxySize = Coord.maxSize, Coord.maxSize
 	
 	def __init__(self, pos, size):
+		'should be values between 1 and galaxySize as position and values not bigger then galaxySize as size'
 		self.pos = pos
 		self.size = size
 	
+	def __getitem__(self, index):
+		'return coordinates like if they are tuple pair'
+		if index==0:
+			return self.pos
+		return self.size
+	
+	def __eq__(self, r):
+		'compare with the same object type for equality'
+		return self.pos == r[0] and self.size == r[1]
+
+	def __neq__(self, c):
+		'compare with the same object type for equality'
+		return not self==c
+
+	def __str__(self):
+		return '%s %s'%(self.pos, self.size)
+		
 	def rects(self):
 		'split the rect into continious rects ( no crossing borders of the game field )'
 		
+		topRight = self.pos+self.size
+		
 		#check if entire rect is inside the galaxy rect, just return it in this case
-		if self.pos+self.size < galaxySize:
+		if topRight <= self.galaxySize:
 			yield self.pos,self.size
-			return
-			
+			return			
 
+		#otherwise, ok take the top-left corner ( before the border )
+		topRightShrink = topRight.shrink(self.galaxySize)
+		sizeTopLeft = topRightShrink-self.pos
+		yield self.pos, sizeTopLeft
+		
+		ext = topRight-self.galaxySize
+		#print 'minimized %s to %s'%(topRight, ext,)
+		if ext.x > 0:
+			rightPos  = Coord(1, self.pos.y)
+			rightSize = ext.x,self.size[1]
+			yield rightPos, rightSize
+		if ext.y > 0:
+			leftPos  = Coord(self.pos.x, 1)
+			leftSize = self.size[0], ext.y
+			yield leftPos, leftSize
+		
+		if ext.x>0 and ext.y>0:
+			yield Coord(1,1), ext
--- test/test_geometry.py
+++ test/test_geometry.py
@@ -1,6 +1,6 @@
 import sys
 sys.path.append('../src')
-from geometry import Coord
+from geometry import Coord, GameRect
 import unittest
 
 class CoordTest(unittest.TestCase):
@@ -61,5 +61,64 @@ class CoordTest(unittest.TestCase):
 		self.assertEqual(b, Coord(0,1))
 		self.assertEqual(d, Coord(3,-1))
 
+class GameRectTest(unittest.TestCase):
+	
+	def test_cmp(self):
+		rc = GameRect(Coord(1,1),(50,120))
+			
+		self.assertNotEqual(GameRect((1,1), (50,121)), rc)
+		self.assertNotEqual(GameRect((1,2), (50,120)), rc)
+		
+	def test_split(self):
+		
+		# completly inside
+		inside = GameRect(Coord(1,1),(50,120))
+		insIter = iter(inside.rects())
+		r = next(insIter)
+		self.assertEqual(r, inside)
+		
+		# extend to the right
+		left = Coord(200, 400)
+		right = GameRect(left, (1100, 300))
+		it = iter(right.rects())
+		riCenter = next(it)
+		
+		self.assertEqual(riCenter[0], left)
+		self.assertEqual(riCenter[1], (800, 300))
+		
+		riRight = next(it)
+		self.assertEqual(riRight[0], Coord(1, 400))
+		self.assertEqual(riRight[1], (300,300))
+		
+		# extend to the bottom
+		bottom = GameRect(left, (333, 999))
+		it = iter(bottom.rects())
+		center = next(it)
+
+		self.assertEqual(center[0], left)
+		self.assertEqual(center[1], (333, 600))
+
+		btm = next(it)
+		self.assertEqual(btm[0], Coord(left.x, 1))
+		self.assertEqual(btm[1], (333,399))
+		
+		# extend to both sides
+		both = GameRect(left, (922, 655))
+		it = iter(both.rects())
+		ins = next(it)
+		self.assertEqual(ins, (left, (800, 600)))
+		
+		right = next(it)
+		self.assertEqual(right, ((1, left.y), (122, 655)))
+		
+		bottom = next(it)
+		self.assertEqual(bottom, ((left.x, 1), (922, 55)))
+		
+		corner = next(it)
+		self.assertEqual(corner, ((1,1), (122, 55)))
+		
+
 if __name__ == '__main__':
     unittest.main()
+
+self.assertEqual(riCenter[0], left)
